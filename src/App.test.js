8b0cfd81b01b421529ff7aa13a8666060643a685
GarenWang <2270099149@qq.com>
2019-08-18 16:09:14 +0800

    Enzyme的配置和使用

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  const container = wrapper.find('[data-test="container"]');
+  // expect(container.length).toBe(1);
+  // // console.log(wrapper.debug());
+  // expect(container.prop('title')).toBe('garen');
+  expect(container).toExist();
+  expect(container).toHaveProp('title', 'garen');
+  expect(wrapper).toMatchSnapshot();
 });
